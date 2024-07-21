af3b35e2e0fe8ca4965f2c1db8dc3c9230251822
Kun Woo Kim <kimkuns98@gmail.com>
2024-07-21 23:11:14 +0900

    prisma test

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,39 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
 app.get("/", (_req, res) => {
     return res.send("Express Typescript on Vercel");
 });
+app.get("/users", (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const users = yield prisma.user.findMany();
+    res.json(users);
+}));
+app.post("/users", (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const user = yield prisma.user.create({
+        data: {
+            name: "Kun Woo Kim",
+            email: "kimkuns98@gmail.com",
+            password: "password",
+        },
+    });
+    res.json(user);
+}));
 app.get("/ping", (_req, res) => {
     return res.send("pong 🏓");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
 import express, { Application, Request, Response } from "express";
 
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 const app: Application = express();
 const port = process.env.PORT || 8080;
 
 app.get("/", (_req: Request, res: Response) => {
   return res.send("Express Typescript on Vercel");
+});
+
+app.get("/users", async (_req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.post("/users", async (_req: Request, res: Response) => {
+  const user = await prisma.user.create({
+    data: {
+      name: "Kun Woo Kim",
+      email: "kimkuns98@gmail.com",
+      password: "password",
+    },
+  });
+  res.json(user);
 });
 
 app.get("/ping", (_req: Request, res: Response) => {
