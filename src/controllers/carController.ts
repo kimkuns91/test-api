0dd1eb43bec766c71a5099fc813c0acd9ad04d8a
Kun Woo Kim <kimkuns98@gmail.com>
2024-07-22 12:13:51 +0900

    push

--- src/controllers/carController.ts
+++ src/controllers/carController.ts
@@ -25,59 +25,68 @@ const getCarInfo = async (req: Request, res: Response) => {
 
     // 기존 차량 정보가 없을 경우 외부 API를 호출합니다.
     if (!process.env.DATA_HUB_API_TOKEN) {
-      return res.status(500).json({ message: "API token is missing in environment variables" });
+      return res
+        .status(500)
+        .json({ message: "API token is missing in environment variables" });
     }
 
-    const response = await axios.post(
-      "https://datahub-dev.scraping.co.kr/assist/common/carzen/CarAllInfoInquiry",
-      {
-        REGINUMBER,
-        OWNERNAME,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: process.env.DATA_HUB_API_TOKEN,
+    try {
+      const response = await axios.post(
+        "https://datahub-dev.scraping.co.kr/assist/common/carzen/CarAllInfoInquiry",
+        {
+          REGINUMBER,
+          OWNERNAME,
         },
-      }
-    );
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: process.env.DATA_HUB_API_TOKEN,
+          },
+        }
+      );
 
-    const data = response.data;
+      const data = response.data;
 
-    console.log("data:", data);
-    // 외부 API 응답 데이터를 데이터베이스에 저장합니다.
-    const newCarInfo = await prisma.car.create({
-      data: {
-        REGINUMBER: REGINUMBER,
-        OWNERNAME: OWNERNAME,
-        CARVENDER: data.data.CARVENDER,
-        CARNAME: data.data.CARNAME,
-        SUBMODEL: data.data.SUBMODEL,
-        UID: data.data.UID,
-        CARYEAR: data.data.CARYEAR,
-        DRIVE: data.data.DRIVE,
-        FUEL: data.data.FUEL,
-        PRICE: data.data.PRICE,
-        CC: data.data.CC,
-        MISSION: data.data.MISSION,
-        CARURL: data.data.CARURL,
-        VIN: data.data.VIN,
-        RESULT: data.data.RESULT,
-        ERRMSG: data.data.ERRMSG,
-        FRONTTIRE: data.data.FRONTTIRE,
-        REARTIRE: data.data.REARTIRE,
-        EOILLITER: data.data.EOILLITER,
-        WIPER: data.data.WIPER,
-        SEATS: data.data.SEATS,
-        FUELECO: data.data.FUELECO,
-        FUELTANK: data.data.FUELTANK,
-        BATTERYLIST: data.data.BATTERYLIST,
-      },
-    });
+      console.log("data:", data);
+      // 외부 API 응답 데이터를 데이터베이스에 저장합니다.
+      const newCarInfo = await prisma.car.create({
+        data: {
+          REGINUMBER: REGINUMBER,
+          OWNERNAME: OWNERNAME,
+          CARVENDER: data.data.CARVENDER,
+          CARNAME: data.data.CARNAME,
+          SUBMODEL: data.data.SUBMODEL,
+          UID: data.data.UID,
+          CARYEAR: data.data.CARYEAR,
+          DRIVE: data.data.DRIVE,
+          FUEL: data.data.FUEL,
+          PRICE: data.data.PRICE,
+          CC: data.data.CC,
+          MISSION: data.data.MISSION,
+          CARURL: data.data.CARURL,
+          VIN: data.data.VIN,
+          RESULT: data.data.RESULT,
+          ERRMSG: data.data.ERRMSG,
+          FRONTTIRE: data.data.FRONTTIRE,
+          REARTIRE: data.data.REARTIRE,
+          EOILLITER: data.data.EOILLITER,
+          WIPER: data.data.WIPER,
+          SEATS: data.data.SEATS,
+          FUELECO: data.data.FUELECO,
+          FUELTANK: data.data.FUELTANK,
+          BATTERYLIST: data.data.BATTERYLIST,
+        },
+      });
 
-    console.log("newCarInfo:", newCarInfo);
+      console.log("newCarInfo:", newCarInfo);
 
-    return res.status(200).json(newCarInfo);
+      return res.status(200).json(newCarInfo);
+    } catch (error) {
+      console.error("Error fetching car info:", error);
+      return res
+        .status(500)
+        .json({ message: "Error fetching car info", error });
+    }
   } catch (error) {
     console.error("Error fetching car info:", error);
     return res.status(500).json({ message: "Error fetching car info", error });
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`This Server is listening on ${port}`);
 });
+
